b2469680a46e75e2ad7807e63b4c639f916c0f2c
A1exAK <121604147+a1ex-ak@users.noreply.github.com>
2025-03-12 15:57:09 +0300

    Update user.js

--- configuration/user.js
+++ configuration/user.js
@@ -10,7 +10,3 @@ user_pref("browser.tabs.drawInTitlebar", true);
 
 // Set UI density to normal
 user_pref("browser.uidensity", 0);
-
-// We remove the inscription "not secure connection", leaving only the icon
-user_pref("security.insecure_connection_text.enabled", false);
-user_pref("security.insecure_connection_text.pbmode.enabled", false);
